436cb84591cb00895a27fe3a8379f82bd57d77f9
TSG <144003249+IrinaAndreeva932221@users.noreply.github.com>
2023-09-28 01:56:48 +0700

    Update main.cpp

--- algorithms/dijkstra/main.cpp
+++ algorithms/dijkstra/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <time.h>
 
+void Swap(int *x, int *y)
+{
+    int tmp=*x; *x=*y; *y=tmp;
+}
+
+void BubbleSortUp (int *a, int n, int g)
+{
+    for(int k=n-1; k>g; k--)
+    {for(int i=g; i<k; i++)
+        if(a[i]>a[i+1])
+        Swap(&a[i], &a[i+1]);
+    }
+}
+
 void RandInput(int **matr, int n, int left = 1, int right = 20)
 {
     int i, j;
@@ -31,23 +45,49 @@ void OutputPath(int *path, int n)
 {
     for (int i = 0; i<n; i++)
         std::cout << path[i] << ' ';
+    std::cout<<std::endl;
 }
 
+void getNewPath(int *path, int n, int *answ)
+{
+   int i, j, p=1, l,temp;
+    for (l=1; l<n; l++) p*=l;
+    for (l = 0; l<p-1; l++)
+    {
+    for(i=n-2; i>0; i--)
+    {if (path[i]<path[i+1])
+    {
+        for(j=n-1; j>i; j--)
+        {
+            if(path[i]<path[j])
+            {temp=path[i];
+            path[i]=path[j];
+            path[j]=temp;
+            break;}
+        }
+        break;
+    }}
+    BubbleSortUp(path, n, i+1);
+    OutputPath(path, n);
+    }
+}
 
 
 int main()
 {
-    int N, first_town, i, j;
+    int N, startTown, i, j;
     std::cout << "Введите количество городов, которые необходимо посетить :";
     std::cin >> N;
     do {
     std::cout << "Введите номер начального города (он не может быть больше их количества):";
-    std::cin >> first_town;} while (first_town>N || first_town<1);
+    std::cin >> startTown;} while (startTown>N || startTown<1);
     int *path = new int[N];
-    path[0] = first_town;
+    int *final_path = new int [N];
+    path[0] = startTown;
+    final_path[0] = startTown;
     for (i = 1, j=1; i<N; i++, j++)
     {
-        if (i == first_town) j++;
+        if (i == startTown) j++;
         path[i] = j;
     }
     int **cost = new int* [N];
@@ -56,9 +96,11 @@ int main()
     RandInput(cost, N);
     OutputPath(path, N);
     std::cout << std::endl;
+    getNewPath(path, N, path);
     OutputMatr(cost, N);
     for (i = 0; i <= N; i++)
         delete [] cost[N];
     delete [] path;
+    delete [] final_path;
     return 0;
 }
